2029ba028fc95b686a7f48028ae81473d3befb55
sibevara <sibevara@LIN24010725.corp.capgemini.com>
2022-06-20 12:30:10 +0530

    changes done in problem1

--- java/src/corejava/Problem1.java
+++ java/src/corejava/Problem1.java
@@ -2,48 +2,30 @@ package corejava;
 
 import java.util.Scanner;
 
-
-
 public class Problem1 {
 
+	public static void main(String[] args) {
 
+		int num, originalNumber, remainder, result = 0;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter any number: ");
 
-public static void main(String[] args) {
+		num = sc.nextInt();
+		System.out.println("You Entered: " + num);
 
+		originalNumber = num;
 
+		while (originalNumber != 0) {
+			remainder = originalNumber % 10;
+			result += Math.pow(remainder, 3);
+			originalNumber /= 10;
 
-int num, originalNumber, remainder, result = 0;
-Scanner sc = new Scanner(System.in);
-System.out.println("Enter any number: ");
+		}
+		if (result == num)
+			System.out.println(num + " is an Armstrong number.");
+		else
+			System.out.println(num + " is not an Armstrong number.");
 
-
-
-num = sc.nextInt();
-System.out.println("You Entered: " + num);
-
-
-
-originalNumber = num;
-
-
-
-while (originalNumber != 0) {
-remainder = originalNumber % 10;
-result += Math.pow(remainder, 3);
-originalNumber /= 10;
-
-
-
-}
-if (result == num)
-System.out.println(num + " is an Armstrong number.");
-else
-System.out.println(num + " is not an Armstrong number.");
-
-
-
-}
-
-
+	}
 
 }
